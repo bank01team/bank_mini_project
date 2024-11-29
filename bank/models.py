e82f815ebc8ce3357af7d17e7f688f3921e5330f
wkdsh21 <wkdsh21@github.com>
2024-11-29 18:08:32 +0900

    mission 2 clear

---
+++ bank/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from member.models import Users
+
+
+# Create your models here.
+class Accounts(models.Model):
+    account_number = models.CharField(max_length=50, unique=True)
+    bank_code = models.CharField(max_length=10)
+    balance = models.IntegerField()
+    account_type = models.CharField(max_length=20)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
+class DepositWithdraw(models.Model):
+    class DepositWithdrawType(models.TextChoices):
+        DEPOSIT = "입금", "입금"  # (데이터베이스 값, 표시 레이블)
+        WITHDRAW = "출금", "출금"
+
+
+class Transaction(models.Model):
+    class TransactionType(models.TextChoices):
+        CASH = "현금", "현금"
+        TRANSFER = "계좌이체", "계좌이체"
+        AUTO_TRANSFER = "자동이체", "자동이체"
+        CARD = "카드결제", "카드결제"
+
+
+class TransactionHistory(models.Model):
+    deposit = models.IntegerField()
+    withdraw = models.IntegerField()
+    balance = models.IntegerField()
+    details = models.CharField(max_length=50)
+    dw_type = models.CharField(max_length=10, choices=DepositWithdraw.DepositWithdrawType)
+    ts_type = models.CharField(max_length=10, choices=Transaction.TransactionType)
+    td_date = models.DateField()
+    account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+
+
+# textchoice상속받은 클래스만들어서 사용
--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # own_apps
+    "bank.apps.BankConfig",
+    "member.apps.MemberConfig",
 ]
 
 MIDDLEWARE = [
@@ -76,7 +79,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": "bank",
         "USER": os.environ.get("BANK_DB_USER_NAME", "postgres"),
         "PASSWORD": os.environ.get("BANK_DB_USER_PASSWORD", "0000"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
@@ -125,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "member.Users"
