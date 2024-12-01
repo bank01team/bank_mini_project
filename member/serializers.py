0f9b78d71e31a5f5046fbfad9cd2741684e376db
wkdsh21 <wkdsh21@github.com>
2024-12-01 23:54:37 +0900

    pull

--- member/serializers.py
+++ member/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import Users
 
 class UserGetUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        exclude=['password',"is_active","is_staff","is_admin","last_login"]
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
 
 class RegisterSerializer(
     serializers.ModelSerializer[Users]
@@ -25,7 +32,3 @@ class RegisterSerializer(
     #     )
     #     return user
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Users
-        fields = '__all__'
